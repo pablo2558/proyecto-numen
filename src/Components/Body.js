a96e9a8e41fe0f707a07c8a1f74b8d48af315257
Pablo Caneo <pablocaneo9@gmail.com>
2022-04-05 20:49:08 -0300

    producto sacado

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import ProductMap from './Carrito/ProductMap'
+
 import Main from './Main/Main'
 
 const Body = () => {
   return (
     <div>
         <Main/>
-        <ProductMap />
     </div>
   )
 }
