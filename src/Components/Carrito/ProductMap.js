c8a980b54d8cba078507b7df6241337f0b877349
Pablo Caneo <pablocaneo9@gmail.com>
2022-04-10 17:51:44 -0300

    seccion producto y redragon ahora funcionan correctamente con el carrito

--- src/Components/Carrito/ProductMap.js
+++ src/Components/Carrito/ProductMap.js
@@ -4,10 +4,11 @@ import { CartContainer } from './ShoppingCart';
 
 const ProductMap = () => {
     const {products, addToCart} = useContext(CartContainer)
+    const productos = products.filter(product => product.id < 21)
   return (
     <div className='lg:pt-40 md:pt-40 sm:pt-40'>
       <div className='flex justify-evenly flex-wrap'>
-        {products.map((product) => (
+        {productos.map((product) => (
           <Product key={product.id} data={product} addToCart={addToCart} />
         ))}
       </div>
